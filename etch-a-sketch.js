70efdcc505765439d19e577099ede799794830d4
friedkaiju <61223378+friedkaiju@users.noreply.github.com>
2024-09-27 16:04:43 +1000

    Built out functionality
    - Generate the canvas with a number of squares
    - Wipe the canvas from view
    - Drag mouse over squares to draw
    
    However the wiping function doesn't truly work and the grid won't generate the correct number of grid square per side.
    
    Fix at a later date maybe

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,22 +3,30 @@ const button = document.querySelector("button")
 const parentGridContainer = document.querySelector("#parentGridContainer")
 const gridContainer = document.querySelector("#gridContainer")
 
-button.addEventListener("click", () => {
+generateButton.addEventListener("click", () => {
     let userGridRow = parseInt(prompt("how many squares per side should the grid be?", "16"), 10)
     generateCanvas(userGridRow)
     }
 )
-//create a container for a 16x16 grid of square divs using only dom techniques
+
+let gridSquare = document.createElement("div")
+
+wipeButton.addEventListener("click", () => {
+    gridRow.remove();
+    gridSquare.style.backgroundColor = 'rgb(255, 255, 255)'
+    }
+)
+
+const gridRow = document.createElement("div");
+gridRow.classList.add("gridRow");
 
 const generateCanvas = (userGridRow) =>  {
     for (let i = 0; i < userGridRow; i++) {
         for (let j = 0; j < userGridRow; j++) {
     //creates 16x16 divs in a row
-            const gridRow = document.createElement("div");
-            gridRow.classList.add("gridRow");
             gridContainer.appendChild(gridRow)
     //creates a div to contain the row and appends it to the container
-            const gridSquare = document.createElement("div");
+            let gridSquare = document.createElement("div");
             gridSquare.classList.add("gridSquare");
             gridRow.appendChild(gridSquare)
             gridSquare.addEventListener("mouseover", () => {
